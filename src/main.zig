9a35decbceb6522fefa223758d8e439f45c1cba7
Indithem <128889127+Indithem@users.noreply.github.com>
2025-08-21 04:43:31 +0530

    runtime [*c][*c]const u8

--- src/main.zig
+++ src/main.zig
@@ -8,9 +8,14 @@ const glfw = @cImport({
 });
 
 const dbg_print = std.debug.print;
-
+var allocator_init = std.heap.DebugAllocator(.{}).init;
+const allocator = allocator_init.allocator();
 
 pub fn main() !void {
+    defer {
+        if (!allocator_init.detectLeaks())
+            dbg_print("No memory leaks found.\n", .{});
+    }
 
     try glfw_raise(GLFWErrors.InitializingError, glfw.glfwInit());
     defer glfw.glfwTerminate();
@@ -24,32 +29,62 @@ pub fn main() !void {
 
     std.debug.print("Initialized GLFW, window={}.\n", .{window});
 
-    std.debug.print("Vulkan extensions supported (count) = {}\n", .{
-        val:{
-            var extensions_count: u32 = undefined;
-            try vk_raise(VulkanErrors.UnknownError, vk.vkEnumerateInstanceExtensionProperties(null, &extensions_count, null));
-            break :val extensions_count;
-        }
-    });
+    // std.debug.print("Vulkan extensions supported (count) = {}\n", .{
+    //     val:{
+    //         var extensions_count: u32 = undefined;
+    //         try vk_raise(VulkanErrors.UnknownError, vk.vkEnumerateInstanceExtensionProperties(null, &extensions_count, null));
+    //         break :val extensions_count;
+    //     }
+    // });
 
     // PRETTYFYME:
     // struct type outside the val: block is preffered?
-    const glfw_extensions = val:{
+    const glfw_extensions: [][*c]const u8 = val:{
         var count: u32 = undefined;
         const c_ext = glfw.glfwGetRequiredInstanceExtensions(&count);
 
-        dbg_print("GLFW extensions needed (count) = {d}\n", .{count});
-        for (c_ext, 0..count) |cstr, i| {
-            dbg_print("    {d}.{s}\n", .{i+1, cstr});
-        }
+        // dbg_print("GLFW extensions needed (count) = {d}\n", .{count});
+        // for (c_ext, 0..count) |cstr, i| {
+        //     dbg_print("    {d}.{s}\n", .{i+1, cstr});
+        // }
 
-        break :val struct {count: u32, extensions: [*][*c]const u8} {
-            .count = count,
-            .extensions = c_ext,
-        };
+        var extensions:[][*c]const u8 = undefined;
+        extensions.len = count;
+        extensions.ptr = c_ext;
+        break :val extensions;
     };
 
     var vk_instance: vk.VkInstance = undefined;
+
+    const available_vk_layers: []vk.VkLayerProperties = val:{
+        var count: u32 = undefined;
+        try vk_raise(VulkanErrors.UnknownError, vk.vkEnumerateInstanceLayerProperties(&count, null));
+        const slice: []vk.VkLayerProperties = try allocator.alloc(vk.VkLayerProperties, count);
+        try vk_raise(VulkanErrors.UnknownError, vk.vkEnumerateInstanceLayerProperties(&count, slice.ptr));
+
+        // dbg_print("All {} available validation layers:\n", .{count});
+        // for (slice, 0..) |prop, i| {
+        //     dbg_print("{d}-layerName= {s} specVersion={d} implementationVer={d} description={s}\n",
+        //         .{i+1, prop.layerName, prop.specVersion, prop.implementationVersion, prop.description});
+        // }
+
+        break :val slice;
+    };
+    defer allocator.free(available_vk_layers);
+
+    const validation_layers = [_][:0]const u8{
+        "VK_LAYER_KHRONOS_validation",
+        "VK_LAYER_MESA_device_select",
+    };
+    for (validation_layers) |layer| {
+        if (!for (available_vk_layers) |avail| {
+            if (std.mem.eql(u8, avail.layerName[0..layer.len], layer))
+                break true;
+        } else false) {
+            dbg_print("Validation Layer = {s} is not available!\n", .{layer});
+            return VulkanErrors.NoValidationSupport;
+        }
+    }
 
     try vk_raise(VulkanErrors.InitializingError, vk.vkCreateInstance(
         &vk.VkInstanceCreateInfo{
@@ -63,9 +98,16 @@ pub fn main() !void {
                 .pEngineName = "never used",
                 // .pNext = null,
             },
-            .enabledExtensionCount = glfw_extensions.count,
-            .ppEnabledExtensionNames = glfw_extensions.extensions,
-            .enabledLayerCount = 0,
+            .enabledExtensionCount = @intCast(glfw_extensions.len),
+            .ppEnabledExtensionNames = glfw_extensions.ptr,
+            .enabledLayerCount = validation_layers.len,
+            .ppEnabledLayerNames = blk: {
+                var ret:[validation_layers.len][*]const u8 = undefined;
+                for (validation_layers, 0..) |lay, i| {
+                    ret[i] = lay.ptr;
+                }
+                break :blk (&ret);
+            },
             // .pNext = null,
         },
         // Allocator, and VkInstance
@@ -77,7 +119,7 @@ pub fn main() !void {
 
 
     while (glfw.glfwWindowShouldClose(window) == 0) : ({
-        glfw.glfwPollEvents();
+        glfw.glfwPollEvdestroyents();
     }) {
         break;  //FIXME: change
     }
@@ -87,10 +129,12 @@ pub fn main() !void {
 }
 
 
+
 const VulkanErrors = error {
     UnknownError,
     InitializingError,
     ExtensionNamesError,
+    NoValidationSupport,
 };
 
 const GLFWErrors = error {
@@ -101,6 +145,7 @@ const GLFWErrors = error {
 fn vk_raise(comptime err_type: VulkanErrors, expr_result:vk.VkResult) VulkanErrors!void {
     return switch (expr_result) {
         vk.VK_SUCCESS => {},
+        vk.VK_ERROR_LAYER_NOT_PRESENT => VulkanErrors.NoValidationSupport,
         vk.VK_ERROR_EXTENSION_NOT_PRESENT => VulkanErrors.ExtensionNamesError,
         else => brk:{
             std.debug.print("vk_raise error: {}\n", .{expr_result});
